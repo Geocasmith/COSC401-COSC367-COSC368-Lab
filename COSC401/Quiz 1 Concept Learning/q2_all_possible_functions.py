e506a72d38f06450940789b4edfab46294b42622
george <gca73@uclive.ac.nz>
2022-03-15 23:29:59 +1300

    Assignment 1

--- COSC401/Quiz 1 Concept Learning/q2_all_possible_functions.py
+++ COSC401/Quiz 1 Concept Learning/q2_all_possible_functions.py
@@ -1,20 +1,37 @@
 import itertools
 
-def all_possible_functions(input_space):
+#Gets the power set of the input space (all possible combinations)
+def get_power_set(input_space):
    powerset = set()
-   for i in range(0, len(input_space)+1):
+   input_length= len(input_space)+1
+   for i in range(0,input_length):
        for subset in itertools.combinations(input_space, i):
            powerset.add(subset)
    return powerset
 
+#Goes through all of the input space and generates all possible functions (given that input set, what are the functions that can return any combination of true and false
+def all_possible_functions(input_space):
+   powerset = get_power_set(input_space) # get the power set of the input space
+   functions = [] # list of all possible functions
 
-X = {"green", "purple"} # an input space with two elements
-F = all_possible_functions(X)
+   for subset in powerset:
+       def function(input, s=subset):
+           return input in s
+
+       functions.append(function)
+   functions=list(dict.fromkeys(functions))
+   return functions
+
+
+X = {"green", "purple"}
+F = get_power_set(X)
 print(F)
+F = all_possible_functions(X)
+print(len(F))
 # Let's store the image of each function in F as a tuple
 images = set()
 for h in F:
-   images.add(tuple(h(x) for x in X))
+   images.add(tuple(h(x) for x in X))#go through each x and get the result from each function and make a tuple
 
 for image in sorted(images):
    print(image)
